ce4ddaf99269f7f7aed0978a446cc6729b66fa94
Omar Rasheed <oyr2@cornell.edu>
2020-03-05 22:53:05 -0500

    Create sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { HomePage } from './components';
+import reducer from './redux/reducer';
+
+class App extends React.Component {
+  store = createStore(reducer);
+
+  render() {
+    return (
+      <Provider store={this.store}>
+        <HomePage />
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SideBar from '../SideBar';
+
+const HomePage: React.FunctionComponent = () => (
+  <div className="container">
+    <SideBar />
+  </div>
+);
+
+export default HomePage;

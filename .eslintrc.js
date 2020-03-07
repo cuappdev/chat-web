9a961bd8e97a5932f49279021aea7b70293c730c
Omar Rasheed <36868927+Omarrasheed@users.noreply.github.com>
2020-03-07 12:01:18 -0500

    Highlight section when selected (#4)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,9 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
+  rules: {
+    // Get rid of these two once we wanna deal w accessibility stuff
+    'jsx-a11y/interactive-supports-focus': 0,
+    'jsx-a11y/click-events-have-key-events': 0
+  }
 };
